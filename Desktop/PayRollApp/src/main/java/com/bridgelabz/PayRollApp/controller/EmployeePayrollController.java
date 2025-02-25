73e8476378a01181a2be30a4dfe9aaa92494c5b0
sumit kumar mahto <kumarmahtosumit45@gmail.com>
2025-02-25 13:13:25 +0530

    [Sumit]add.adding UC1

---
+++ Desktop/PayRollApp/src/main/java/com/bridgelabz/PayRollApp/controller/EmployeePayrollController.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.PayRollApp.controller;
+
+import com.bridgelabz.PayRollApp.dto.EmployeePayrollData;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeePayrollController {
+
+    private List<EmployeePayrollData> employeeList = new ArrayList<>();
+
+    @GetMapping
+    public List<EmployeePayrollData> getAllEmployees() {
+        return employeeList;
+    }
+
+    @PostMapping
+    public EmployeePayrollData addEmployee(@RequestBody EmployeePayrollData employee) {
+        employee.setId(employeeList.size() + 1);
+        employeeList.add(employee);
+        return employee;
+    }
+
+    @GetMapping("/{id}")
+    public EmployeePayrollData getEmployeeById(@PathVariable int id) {
+        return employeeList.stream()
+                .filter(emp -> emp.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
+
